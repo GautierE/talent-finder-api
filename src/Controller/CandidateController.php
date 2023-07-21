e1f2433f7e6f4898daf74785cd96e3d2a4093c17
Gautier <gautier.edel.info@gmail.com>
2023-07-21 13:32:28 +0200

    feat(matchCandidates) : match route parse json request content

---
+++ src/Controller/CandidateController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\CandidateService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/candidate')]
+class CandidateController extends AbstractController
+{
+    #[Route('/match', name: 'match_candidates', methods: ['POST'])]
+    public function matchCandidates(Request $request, CandidateService $candidateService): Response
+    {
+        $requestData = json_decode($request->getContent(), true);
+
+        $location = $requestData['location'] ?? null;
+        $isRemote = $requestData['isremote'] ?? null;
+        $experience = $requestData['experience'] ?? null;
+        $salary = $requestData['salary'] ?? null;
+
+        $jobRequirements = [
+            'location' => $location,
+            'isRemote' => $isRemote,
+            'experience' => $experience,
+            'salary' => $salary,
+        ];
+
+        $matchingCandidates = $candidateService->matchCandidates($jobRequirements);
+
+        return $this->json($matchingCandidates);
+    }
+}
---
+++ src/Service/CandidateService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Candidate;
+
+class CandidateService
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function matchCandidates(array $jobRequirements): array
+    {
+        $matchingCandidates = $this->entityManager
+            ->getRepository(Candidate::class)
+            ->findAll();
+
+        return $matchingCandidates;
+    }
+}
